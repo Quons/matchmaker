9f1353d97d79a2bdc95d0b6d3f23bb8a1a92fdcd
liuyc <liuyc@didichuxing.com>
2021-08-30 20:26:25 +0800

    删除无用的文件

--- routers/router.go
+++ routers/router.go
@@ -6,11 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 	//启用swagger文档
 	_ "github.com/Quons/matchmaker/docs"
-	"github.com/Quons/matchmaker/middleware"
 	"github.com/Quons/matchmaker/pkg/logging"
 	"github.com/Quons/matchmaker/pkg/setting"
-	"github.com/Quons/matchmaker/routers/api"
-	"github.com/Quons/matchmaker/routers/api/v1"
 	"github.com/Quons/matchmaker/routers/matchmaker"
 	"github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -18,48 +15,6 @@ import (
 
 /*路由注册*/
 func registerRouter(r *gin.Engine) {
-	r.GET("/auth", api.GetAuth)
-	/*r.GET("/", func(c *gin.Context) {
-		name := c.Query("name")
-		logrus.Info(name)
-		time.Sleep(20 * time.Second)
-		c.String(http.StatusOK, "welcome Gin Server:%s\n", name)
-	})*/
-	r.POST("/upload", api.UploadImage)
-
-	brandApi := r.Group("/brand")
-
-	{
-		brandApi.POST("/addBrand", v1.AddBrand)
-		brandApi.POST("/getBrandList", v1.GetBrandList)
-		brandApi.POST("/deleteBrand", v1.DeleteBrand)
-	}
-
-	apiv1 := r.Group("/api/v1")
-
-	//验证token
-	//apiv1.Use(jwt.JWT())
-	apiv1.Use(middleware.CheckToken())
-	{
-		//获取文章列表
-		apiv1.GET("/articles", v1.GetArticles)
-		//获取指定文章
-		apiv1.GET("/articles/:id", v1.GetArticle)
-		//新建文章
-		apiv1.POST("/articles", v1.AddArticle)
-		//新增文章和标签
-		apiv1.GET("/articleAndTag", v1.AddArticleAndTag)
-		//更新指定文章
-		apiv1.PUT("/articles/:id", v1.EditArticle)
-		//删除指定文章
-		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
-		//生成文章海报
-		apiv1.POST("/articles/poster/generate", v1.GenerateArticlePoster)
-	}
-	{
-		apiv1.POST("/getCourse", v1.GetCourse)
-	}
-
 	apiv2 := r.Group("/matchmaker/v1")
 
 	//验证token
